256ea38346e2fea09baf13c55809e2bb6a59f7eb
li.jie <li.jie@sand.com.cn>
2017-10-25 16:50:25 +0800

    阻塞进程 System.in.read();

--- dubbodemo-consumer/src/main/java/com/alibaba/dubbo/consumer/Consumer.java
+++ dubbodemo-consumer/src/main/java/com/alibaba/dubbo/consumer/Consumer.java
@@ -1,8 +1,11 @@
 package com.alibaba.dubbo.consumer;
+import java.io.IOException;
+
 import com.alibaba.dubbo.demo.DemoService;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Consumer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //测试常规服务
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("consumer.xml");
@@ -11,5 +14,6 @@ public class Consumer {
         DemoService demoService = context.getBean(DemoService.class);
         System.out.println("consumer");
         System.out.println(demoService.getPermissions(1L));
+        System.in.read();
     }
 }
